AlejandroSalgadoG/WebApi

add token authentication to user app

app/user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import gettext as translate
 from rest_framework import serializers
 
 
@@ -13,3 +14,28 @@ class UserSerializer(serializers.ModelSerializer):
         # overrites default behavior to encript password before save user
         # otherwise the password is saved directly in plain text
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),  # not used in this method but it is required arg
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = translate("Unable to authenticate with provided credentials.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs

AlejandroSalgadoG/CreightonApp

app/user/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -13,3 +14,28 @@ class UserSerializer(serializers.ModelSerializer):
         # overrites default behavior to encript password before save user
         # otherwise the password is saved directly in plain text
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+
+        user = authenticate(
+            request=self.context.get("request"),  # not used in this method but it is required arg
+            username=email,
+            password=password,
+        )
+
+        if not user:
+            msg = _("Unable to authenticate with provided credentials.")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs